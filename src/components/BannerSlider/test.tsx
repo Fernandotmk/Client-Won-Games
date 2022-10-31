3ca5bb9bfe7891638870263241245f28b64f5d26
Fernandotmk <fernando.tmk@hotmail.com>
2022-10-31 08:39:17 -0300

    Created GameCardSlider and updated BannerSlider

---
+++ src/components/BannerSlider/test.tsx
@@ -0,0 +1,63 @@
+import { render, screen } from '@testing-library/react'
+import 'match-media-mock'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import BannerSlider from '.'
+
+const items = [
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x582',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
+
+describe('<BannerSlider />', () => {
+  it('should render the heading', () => {
+    // são três estapas do teste:
+    // 1- renderizar o componente 'render' (cria um dom)
+    // 2- seleconar o elemento a ser testado 'scren' (queries) - getByLabel...
+    // 3- expect/assertion - comparação / análise (espero que renderize a logo branca)
+
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+    expect(container.querySelector('.slick-vertical')).toBeInTheDocument()
+
+    // expect(container.firstChild).toMatchSnapshot()
+  })
+
+  // renderizar um item por vez
+  it('should render with 1 active item', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+    // verificando se tem 2 slides
+    expect(container.querySelectorAll('.slick-slide')).toHaveLength(2)
+
+    // verificiando se tem um ativo
+    expect(container.querySelectorAll('li.slick-active')).toHaveLength(1)
+
+    expect(
+      screen.getByRole('heading', { name: /defy death 1/i, hidden: false })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /defy death 2/i, hidden: true })
+    ).toBeInTheDocument()
+  })
+
+  it('should render with the dots', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+    expect(container.querySelector('.slick-dots')).toBeInTheDocument()
+  })
+})
---
+++ src/components/GameCardSlider/stories.tsx
@@ -0,0 +1,72 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import GameCardSlider from '.'
+
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x141',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x143',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x144',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x145',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  }
+]
+
+export default {
+  title: 'GameCardSlider',
+  component: GameCardSlider,
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as ComponentMeta<typeof GameCardSlider>
+
+export const Template: ComponentStory<typeof GameCardSlider> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto', paddingTop: '20px' }}>
+    <GameCardSlider {...args} />
+  </div>
+)
+
+// Caso precise clonar
+// export const Basic = Template.bind({})
+// Basic.args = {
+// aqui vai os argumentos de cada Component
+// exemplo: title: 'Teste',
+// }

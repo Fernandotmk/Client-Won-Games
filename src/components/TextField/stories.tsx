bbf324f0401f8e16cce309956fbd7c01e5157a03
Fernandotmk <fernando.tmk@hotmail.com>
2022-11-04 19:11:28 -0300

    Created TextField

---
+++ src/components/TextField/stories.tsx
@@ -0,0 +1,35 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Email } from 'styled-icons/material-outlined'
+import TextField from '.'
+
+export default {
+  title: 'TextField',
+  component: TextField,
+  args: {
+    label: 'E-mail',
+    labelFor: 'Email',
+    id: 'Email',
+    initialValue: '',
+    placeholder: 'john.cage@gmail.com'
+  },
+  argTypes: {
+    onInput: { action: 'changed' }
+  }
+} as ComponentMeta<typeof TextField>
+
+export const Template: ComponentStory<typeof TextField> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+export const WithIcon = Template.bind({})
+WithIcon.args = {
+  icon: <Email />
+}
+
+export const withError = Template.bind({})
+
+withError.args = {
+  error: 'Ops...something is wrong'
+}

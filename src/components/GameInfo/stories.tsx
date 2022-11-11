d5b897e81c8f688f86f0ff77f3fbf85a7fb12437
Fernandotmk <fernando.tmk@hotmail.com>
2022-11-11 18:44:50 -0300

    Created GameInfo

---
+++ src/components/GameInfo/stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import GameInfo from '.'
+import mockGame from './mock'
+
+export default {
+  title: 'GameInfo',
+  component: GameInfo,
+  args: mockGame,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as ComponentMeta<typeof GameInfo>
+
+export const Template: ComponentStory<typeof GameInfo> = (args) => (
+  <div style={{ maxWidth: '144rem', margin: 'auto', padding: '1.5rem' }}>
+    <GameInfo {...args} />
+  </div>
+)
+
+// Caso precise clonar
+// export const Basic = Template.bind({})
+// Basic.args = {
+// aqui vai os argumentos de cada Component
+// exemplo: title: 'Teste',
+// }

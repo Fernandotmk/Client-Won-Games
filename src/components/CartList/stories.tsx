c0ef381bfcfe5218bf098a0ebc266f8e05e23d65
Fernandotmk <fernando.tmk@hotmail.com>
2022-11-17 07:15:58 -0300

    Created CartList

---
+++ src/components/CartList/stories.tsx
@@ -0,0 +1,31 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import CartList, { CartListProps } from '.'
+
+import mockItems from './mock'
+
+export default {
+  title: 'CartList',
+  component: CartList,
+  args: {
+    items: mockItems,
+    total: 'R$ 330,00'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as ComponentMeta<typeof CartList>
+
+export const Template: ComponentStory<typeof CartList> = (args) => (
+  <div style={{ maxWidth: 800 }}>
+    <CartList {...args} />
+  </div>
+)
+
+// Caso precise clonar
+// export const Basic = Template.bind({})
+// Basic.args = {
+// aqui vai os argumentos de cada Component
+// exemplo: title: 'Teste',
+// }

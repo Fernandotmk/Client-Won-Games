ea496ea8d829e6c824be7c3b4ee050da68ded4fd
Fernandotmk <fernando.tmk@hotmail.com>
2023-01-31 14:08:40 -0300

    Created Graphql Queries for Games

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,15 +1,34 @@
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 import filterItemsMock from 'components/ExploreSidebar/mock'
-import GamesMock from 'components/GameCardSlider/mock'
+import { initializeApollo } from 'utils/apollo'
+import { QUERY_GAMES } from 'graphql/games'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query({
+    query: QUERY_GAMES,
+    variables: { limit: 6 }
+  })
+
+  console.log(data.games.data)
+
   return {
     props: {
-      games: GamesMock,
+      revalidate: 60,
+      games: data.games.data.map((game) => ({
+        title: game.attributes.name,
+        developer: game.attributes.developers.data[0].attributes.name,
+        img: `http://localhost:1337${game.attributes.cover.data.attributes.url}`,
+        price: new Intl.NumberFormat('en', {
+          style: 'currency',
+          currency: 'USD'
+        }).format(game.attributes.price)
+      })),
       filterItems: filterItemsMock
     }
   }
